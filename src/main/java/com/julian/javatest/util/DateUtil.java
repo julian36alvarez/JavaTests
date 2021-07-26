e9fdce34bc32450de0e69182e606c4d5df45885c
julian36alvarez <julian36alvarez@gmail.com>
2021-07-26 18:48:46 -0500

    LeapYear

--- src/main/java/com/julian/javatest/util/DateUtil.java
+++ src/main/java/com/julian/javatest/util/DateUtil.java
@@ -2,10 +2,6 @@ package com.julian.javatest.util;
 
 public class DateUtil {
     public static boolean isLeapYear (int year){
-        if(year%400==0){
-            return true;
-        }else {
-            return false;
-        }
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
 }
