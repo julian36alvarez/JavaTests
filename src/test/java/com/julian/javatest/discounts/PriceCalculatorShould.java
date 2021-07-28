9bde517f434e8ad1a1f8390da8e69f349537a4d3
julian36alvarez <julian36alvarez@gmail.com>
2021-07-28 15:12:39 -0500

    Descuento Final

--- src/test/java/com/julian/javatest/discounts/PriceCalculatorShould.java
+++ src/test/java/com/julian/javatest/discounts/PriceCalculatorShould.java
@@ -19,4 +19,13 @@ public class PriceCalculatorShould {
         calculator.addPrice(15.5);
         assertThat(calculator.getTotal(), is(25.7));
     }
+
+    @Test
+    public void apply_discount_to_prices() {
+        PriceCalculator calculator = new PriceCalculator();
+        calculator.addPrice(12.5);
+        calculator.addPrice(17.5);
+        calculator.setDiscount(50);
+        assertThat(calculator.getTotal(), is(15.0));
+    }
 }
